4d77372a659e1f2ab16f3872566c95d940af2c73
Your Name <you@example.com>
2022-01-01 22:25:39 -0500

    create project basic structure

---
+++ src/test/java/manuel/mora/firstproject/FirstprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package manuel.mora.firstproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
